abd50ab425c9dd72ca10fd871fd6793164dca2cc
Nagesh B C <123981161+NageshSiddu296@users.noreply.github.com>
2023-09-04 13:31:24 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,106 @@
+const logotext = "Nagesh";
+const meta = {
+    title: "My-Portfolio",
+    description: "I'm Nagesh B C.",
+};
+
+const introdata = {
+    title: "Hi, I'm Nagesh",
+    animated: {
+        first: "Frontend Web Developer",
+        second: "Frontend Web Developer",
+        third: "Frontend Web Developer",
+        fourth: "Frontend Web Developer",
+        fifth: "Frontend Web Developer",
+        sixth: "Frontend Web Developer",
+        // add more if you'd like but make sure to update /src/pages/home/index.js Line 29
+    },
+    description: "Dedicated technology enthusiast with an insatiable curiosity for unraveling the complexities of our digital world.",
+    your_img_url: "https://i.ibb.co/wRhqgzJ/profile.webp",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "With a deep passion for technology, I completed a front-enddevelopment course at NxtWave to broaden my skillset and explorenew opportunities. I am enthusiastic about applying my understandingof design principles, strong problem-solving abilities, and meticulousattention to detail to web development. My goal is to utilize my skillsand knowledge in a professional setting while contributing toinnovative projects. I thrive as a quick learner, adaptable individual, andgreatly enjoy working collaboratively. Currently, I am actively seeking achallenging role as a front-end developer, where I can continuelearning, growing, and making a meaningful impact.",
+};
+
+
+const skills = [{
+        name: "HTML",
+    },
+    
+    {
+        name: "CSS",
+    },
+    
+    {
+        name: "Bootstrap",
+    },
+    
+    {
+        name: "Javascript",
+    },
+
+    {
+        name: "ReactJS",
+    },
+
+    {
+        name: "Tailwind CSS",
+    },
+
+    {
+        name: "Next.Js",
+    },
+
+    
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/ZNBbkTg/Screenshot-2023-08-30-at-6-45-05-PM.png",
+        description: "Movieflix Movie App",
+        link: "https://movieflix-zoln.vercel.app/",
+    },
+    {
+        img: "https://nordicapis.com/wp-content/uploads/5-Best-Speech-to-Text-APIs-2-e1615383933700.png",
+        description: "Speech To Text Convertor Web App",
+        link: "https://speechtotextwebsite.netlify.app/",
+    },
+    {
+        img: "https://i.ibb.co/rmVBpRS/Screenshot-2023-08-30-at-6-59-55-PM.png",
+        description: "Google Search Clone",
+        link: "https://google-search-clone-kohl.vercel.app/",
+    },
+    {
+        img: "https://i.ibb.co/6HrhhJS/Screenshot-2023-08-30-at-7-03-44-PM.png",
+        description: "Weather App With Forecast and Current Location",
+        link: "https://weatherio-five.vercel.app/",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "nageshsiddu296@gmail.com",
+    description: "Looking forward to hearing from you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_zc6uqcl",
+    YOUR_TEMPLATE_ID: "template_3yg0ce1",
+    YOUR_USER_ID: "eYJye39uDmwD9hmyp",
+};
+
+const socialprofils = {
+    github: "https://github.com/NageshSiddu296",
+    linkedin: "https://www.linkedin.com/in/nagesh-b-c-739bb622a",
+    gmail: "https://gmail.com/"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
